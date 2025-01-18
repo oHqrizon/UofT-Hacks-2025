695ea974619207fcc438dbc4026ccdd3d3ce38ca
oHqrizon <jacksoncai200@gmail.com>
2025-01-18 10:48:57 -0500

    linked flask with react

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, jsonify, render_template, request, url_for, redirect
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=["POST", "GET"])
 def main():
@@ -16,6 +18,19 @@ def main():
 def display(usr): #name as paramater
     selected_option = request.args.get('edu') #recieve edu variable 
     return render_template("start.html", usr=usr, selected_option=selected_option)
+
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    data = request.get_json()
+    name = data.get('name')
+    education = data.get('education')
+
+    print(f"Name: {name}, Education Level: {education}")
+
+    return jsonify({
+        "message": "Form submitted",
+        "submitted_data": data
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
